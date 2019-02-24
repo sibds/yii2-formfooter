e1bcc9959391d50a2871108fe9a67aa7f1695d3a
Vadim Mazur <mazurva@gmail.com>
2019-02-24 20:14:46 +0600

    Update messages.php

--- messages/ru/messages.php
+++ messages/ru/messages.php
@@ -13,4 +13,6 @@ return [
     'Update and close' => 'Сохранить и закрыть',
     'Created' => 'Cоздано',
     'Updated' => 'Изменено',
+    'Delete' => 'Удалить',
+    'Are you sure you want to delete this item?' => 'Вы действительно хотите удалить эту запись?',
 ];
